dbf5dda824fdbe3c0bba56fd794a2b3e747e34d1
jessequartey <jessefquartey@gmail.com>
2023-04-26 00:23:26 +0000

    login UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './lib/routes'
 
-function App() {
+export default function App() {
 
   return (
-    <>
-     
-    </>
+   <ChakraProvider>
+    <RouterProvider router={router}/>
+   </ChakraProvider>
   )
 }
 
-export default App
+
